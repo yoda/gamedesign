bbc00b090579179cab0e4e5cfe838340175984b0
yoda <yoda@tallon4.net>
2010-09-13 10:41:58 +0800

    Toyed with dedicated server and network sync

---
+++ GameEntities/TS Specific/Statue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameEntities
+{
+    /// <summary>
+    /// Defines the <see cref="PlayerCharacter"/> entity type.
+    /// </summary>
+    public class StatueCharacterType : PlayerCharacterType
+    {
+    }
+}
